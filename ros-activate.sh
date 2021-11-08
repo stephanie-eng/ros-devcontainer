047a4cb08f601c886bb8b2a672005cbfdb0540de
Stephanie Eng <stephanie.eng@picknik.ai>
2021-11-08 18:08:18 -0500

    Fixed usermod

--- ros-activate.sh
+++ ros-activate.sh
@@ -4,7 +4,7 @@ cd $CONTAINER_WORKSPACE_FOLDER
 INPUT_GID=$(stat -c "%g" /dev/input/event0)
 INPUT_GROUP_NAME=$(getent group $INPUT_GID | cut -d: -f1)
 if ! id -G | grep -q $INPUT_GID; then
-  usermod -a -G $INPUT_GID $(whoami)
+  sudo usermod -a -G $INPUT_GID $(whoami)
   newgrp $INPUT_GROUP_NAME
 fi
 if [ -f /opt/ros/$ROS_DISTRO/setup.zsh ]; then
